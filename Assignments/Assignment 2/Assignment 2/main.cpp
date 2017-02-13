8e4ef184ece7f3dce4288b1f4bf5256e3853e327
Shawon Ashraf <shawon13@live.com>
2017-02-13 23:43:45 +0600

    Assignement 2 codes

---
+++ Assignments/Assignment 2/Assignment 2/main.cpp
@@ -0,0 +1,144 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+
+/* system function prototypes */
+void drawPixel(int x, int y);
+static void resize(int width, int height);
+static void display(void);
+static void key(unsigned char key, int x, int y);
+static void idle(void);
+
+/* ds */
+
+struct Point {
+    int x;
+    int y;
+};
+
+std::vector<Point> points;
+int numPoints;
+
+
+/* user function prototypes */
+
+void DrawPolygon(std::vector<Point>& points);
+void Rotate();
+
+/* Program entry point */
+
+int main(int argc, char *argv[])
+{
+    std::cin >> numPoints;
+    for(int i = 0; i < numPoints; i++)
+    {
+        Point x;
+        std::cin >> x.x >> x.y;
+        points.push_back(x);
+    }
+    
+    
+    glutInit(&argc, argv);
+    glutInitWindowSize(1024,768);
+    glutInitWindowPosition(10,10);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    
+    glutCreateWindow("assignment 2");
+    
+    glutReshapeFunc(resize);
+    glutDisplayFunc(display);
+    glutKeyboardFunc(key);
+    glutIdleFunc(idle);
+    
+    glClearColor(0,0,0,0);
+    
+//    Init();
+    
+    glutMainLoop();
+    
+    return EXIT_SUCCESS;
+}
+
+
+/* system function implementations */
+
+
+void drawPixel(int x, int y)
+{
+    glBegin(GL_POINTS);
+    glVertex2i(x,y);
+    glEnd();
+}
+
+/* GLUT callback Handlers */
+
+static void resize(int width, int height)
+{
+    const float ar = (float) width / (float) height;
+    
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    //glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
+    glOrtho(-width/2, width/2, -height/2, height/2, -300, 300);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity() ;
+}
+
+static void display(void)
+{
+    int i;
+    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glColor3d(1,1,1);
+    
+    
+    glPushMatrix();
+    DrawPolygon(points);
+    glPopMatrix();
+    
+    glutSwapBuffers();
+}
+
+
+static void key(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+        case 27 :
+        case 'q':
+            exit(0);
+            break;
+    }
+    
+    glutPostRedisplay();
+}
+
+static void idle(void)
+{
+    glutPostRedisplay();
+}
+
+/* user function implementations */
+
+
+void DrawPolygon(std::vector<Point>& points)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1, 0, 0);
+    
+    glBegin(GL_POLYGON);
+    for (int i = 0; i < numPoints; i++) {
+        glVertex2i(points[i].x, points[i].y);
+    }
+    glEnd();
+    glFlush();
+
+}
+
