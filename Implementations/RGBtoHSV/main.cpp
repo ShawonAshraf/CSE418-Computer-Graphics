2c01122d9aeb9d97e409e9b19bc6aba873ea5445
Shawon Ashraf <shawon13@live.com>
2017-04-10 01:32:55 +0600

    Done RGBtoHSV

--- Implementations/RGBtoHSV/main.cpp
+++ Implementations/RGBtoHSV/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <algorithm>
-
-double max(double r, double g, double b);
+#include <vector>
 
 int main() {
     std::cout << "RGB To HSV Converter" << std::endl;
@@ -9,21 +8,43 @@ int main() {
     double r,g,b;
     double h,s,v;
 
+    std::vector<double> rgb;
+
+
     std::cin >> r >> g >> b;
-    double max = max(r, g, b);
 
-    v = max; // got v
+    rgb.push_back(r);
+    rgb.push_back(g);
+    rgb.push_back(b);
 
-    if (max != 0) {
+    auto max = std::max_element(rgb.begin(), rgb.end());
+    auto min = std::min_element(rgb.begin(), rgb.end());
 
+    double delta = *max - *min;
+
+    v = *max; // got v
+
+    if (*max != 0) {
+        s = delta / *max;
+    } else {
+        s = 0;
+        h = -1;
+
+        return 0;
     }
 
+    if (r == *max) {
+        h = (g - b) / delta;
+    } else if (g == *max) {
+        h = (b - r) + 2 / delta;
+    } else {
+        h = (r - b) + 4 / delta;
+    }
+
+    h *= 60;
+    if (h < 0) h += 360;
+
+    std::cout << h << " " << s << " " << v << " " << std::endl;
 
     return 0;
-}
-
-double max(double r, double g, double b) {
-    if(r > g && r > b) return r;
-    else if (g > r && g > b) return g;
-    else return b;
 }
